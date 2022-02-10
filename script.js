5063ef4b2679057b539a9cbb61ca74926d23098f
mum33n <azeezmumeen6@gmail.com>
2022-02-10 02:48:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     var resulthtml=$("#result")
 
     function getData(){
-        $.get("http://5d76bf96515d1a0014085cf9.mockapi.io/quiz", function(data, status){
+        $.get("./quiz.json", function(data, status){
         var response=data;
         for (let i = 0; i < response.length; i++) {
 
