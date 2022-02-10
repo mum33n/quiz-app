4673cb8fe532f9b96f023ef25d0d0e0db0b35b37
mum33n <59535561+mum33n@users.noreply.github.com>
2022-02-10 08:17:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     var resulthtml=$("#result")
 
     function getData(){
-        $.get("./quiz.json/", function(data, status){
+        $.get("https://raw.githubusercontent.com/mum33n/quiz-app/main/quiz.json", function(data, status){
         var response=data;
         for (let i = 0; i < response.length; i++) {
  
